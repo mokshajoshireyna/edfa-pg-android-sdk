06431298ee3b560c268e42249cde41703022f5e4
Ankit Solanki <ankit.solanki@reynasolutions.com>
2024-07-29 17:43:19 +0530

    card pay without ui - in progress

--- edfa-pg-sdk/src/main/java/com/edfapg/sdk/views/edfacardpay/EdfaCardPay.kt
+++ edfa-pg-sdk/src/main/java/com/edfapg/sdk/views/edfacardpay/EdfaCardPay.kt
@@ -10,79 +10,75 @@ import com.edfapg.sdk.model.request.order.EdfaPgSaleOrder
 import com.edfapg.sdk.model.request.payer.EdfaPgPayer
 import com.edfapg.sdk.model.response.sale.EdfaPgSaleResponse
 
-internal var instance:EdfaCardPay? = null
+internal var instance: EdfaCardPay? = null
+
 class EdfaCardPay {
-    constructor(){
+    constructor() {
         instance = this
     }
 
-    var _order:EdfaPgSaleOrder? = null
-    var _payer:EdfaPgPayer? = null
-    var _card:EdfaPgCard? = null
-    var _onTransactionFailure:((EdfaPgSaleResponse?, Any?) -> Unit)? = null
-    var _onTransactionSuccess:((EdfaPgSaleResponse?, Any?) -> Unit)? = null
-    var _onError:((Any) -> Unit)? = null
-    var _onPresent:((Activity) -> Unit)? = null
-    var edfaCardPayTransaction: EdfaCardPayTransaction? = null
+    var _order: EdfaPgSaleOrder? = null
+    var _payer: EdfaPgPayer? = null
+    var _card: EdfaPgCard? = null
+    var _onTransactionFailure: ((EdfaPgSaleResponse?, Any?) -> Unit)? = null
+    var _onTransactionSuccess: ((EdfaPgSaleResponse?, Any?) -> Unit)? = null
+    var _onError: ((Any) -> Unit)? = null
+    var _onPresent: ((Activity) -> Unit)? = null
 
-    fun setOrder(order:EdfaPgSaleOrder) : EdfaCardPay{
+    fun setOrder(order: EdfaPgSaleOrder): EdfaCardPay {
         _order = order
         return this
     }
 
-    fun setPayer(payer:EdfaPgPayer) : EdfaCardPay{
+    fun setPayer(payer: EdfaPgPayer): EdfaCardPay {
         _payer = payer
         return this
     }
 
-    fun setCard(card:EdfaPgCard) : EdfaCardPay{
+    fun setCard(card: EdfaPgCard): EdfaCardPay {
         _card = card
         return this
     }
 
-    fun onTransactionFailure(callback:(EdfaPgSaleResponse?, Any?) -> Unit) : EdfaCardPay{
+    fun onTransactionFailure(callback: (EdfaPgSaleResponse?, Any?) -> Unit): EdfaCardPay {
         _onTransactionFailure = callback
         return this
     }
 
-    fun onTransactionSuccess(callback:(EdfaPgSaleResponse?, Any?) -> Unit) : EdfaCardPay{
+    fun onTransactionSuccess(callback: (EdfaPgSaleResponse?, Any?) -> Unit): EdfaCardPay {
         _onTransactionSuccess = callback
         return this
     }
 
-    fun initialize(context:Context, onError:(Any) -> Unit, onPresent:(Activity) -> Unit){
-        if(_card == null) {
-            _onError = onError
-            _onPresent = onPresent
-            context.startActivity(intent(context, onError, onPresent))
-        }else{
-            edfaCardPayTransaction = EdfaCardPayTransaction(context)
-            edfaCardPayTransaction!!.doSaleTransaction(_payer, _order, _card ) { cardTransactionData ->
-                Log.e("cardTransactionData",cardTransactionData.toString());
-                val intent =
-                    EdfaPgSaleWebRedirectActivity.intent(context, cardTransactionData)
-//                sale3dsRedirectLauncher.launch(intent)
-            }
-        }
+    fun initialize(context: Context, onError: (Any) -> Unit, onPresent: (Activity) -> Unit) {
+        _onError = onError
+        _onPresent = onPresent
+        context.startActivity(intent(context, onError, onPresent))
     }
 
-    fun intent(context:Context, onError:(Any) -> Unit, onPresent:(Activity) -> Unit) : Intent {
+    fun intent(context: Context, onError: (Any) -> Unit, onPresent: (Activity) -> Unit): Intent {
         _onError = onError
         _onPresent = onPresent
 
         val intent = Intent(context, EdfaCardPayActivity::class.java)
-        return  intent
+        if(_card != null){
+            intent.putExtra("cardNumber", _card?.number)
+            intent.putExtra("expireMonth", _card?.expireMonth)
+            intent.putExtra("expireYear", _card?.expireYear)
+            intent.putExtra("cvv", _card?.cvv)
+        }
+        return intent
     }
 
-    fun fragment(onError:(Any) -> Unit, onPresent:(Activity) -> Unit) : Fragment {
+    fun fragment(onError: (Any) -> Unit, onPresent: (Activity) -> Unit): Fragment {
         _onError = onError
         _onPresent = onPresent
 
-        return  EdfaCardPayFragment()
+        return EdfaCardPayFragment()
     }
 
-    companion object{
-        internal fun shared() : EdfaCardPay?{
+    companion object {
+        internal fun shared(): EdfaCardPay? {
             return instance
         }
     }
