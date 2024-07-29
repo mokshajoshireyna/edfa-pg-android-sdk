7d625a0fac89a580443d3327f0c530b71a8ae0e2
Moksha Joshi <moksha.joshi@reynasolutions.com>
2024-07-29 19:41:47 +0530

    push

---
+++ app/src/main/java/com/edfapg/sample/app/Credentials.kt
@@ -0,0 +1,7 @@
+package com.edfapg.sample.app
+
+// testing
+var MERCHANT_KEY: String = "f6534bd9-d4e8-431f-93d2-592b1b112e9b"
+var MERCHANT_PASSWORD: String = "59338f04447a23f15f749a4bd3bb0e9f"
+var PAYMENT_URL: String = "https://api.edfapay.com/payment/post"
+
